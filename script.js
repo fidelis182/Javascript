b379dddf3c28f076bccdd337b48ed4480d225317
fidelma <fedymueni18@gmail>
2023-08-14 15:44:26 +0300

    creating variables and assigning their values assignment

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let counttry = "Nigeria";
+let continent = "Africa";
+let population = "10 million";
+console.log(counttry, continent, population);
 let js = "amazing";
 if (js === "amazing") alert("Javascript is Fun!");
 40 + 8 + 23 - 10;
