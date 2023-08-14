ee17cd29eafd0821df5cfb3656db92371202ef84
fidelma <fedymueni18@gmail>
2023-08-14 16:53:03 +0300

    js data types

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let counttry = "Nigeria";
+// Values & Variables
+let country = "Kenya";
 let continent = "Africa";
 let population = "10 million";
-console.log(counttry, continent, population);
-let js = "amazing";
-if (js === "amazing") alert("Javascript is Fun!");
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+console.log(country, continent, population);
+
+// Data Types
+let isIsland = false;
+let language;
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
