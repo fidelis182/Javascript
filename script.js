4ef2162bbe37a083ff9eb7ffb848ab03125e479d
fidelma <fedymueni18@gmail>
2023-08-15 09:39:41 +0300

    basic operators

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 // Values & Variables
-let country = "Kenya";
-let continent = "Africa";
-let population = "10 million";
-console.log(country, continent, population);
+// let country = "Kenya";
+// let continent = "Africa";
+// let population = "10 million";
+// console.log(country, continent, population);
 
-// Data Types
-let isIsland = false;
-let language;
-console.log(country);
-console.log(continent);
-console.log(population);
-console.log(isIsland);
-console.log(language);
+// // Data Types
+// let isIsland = false;
+// let language;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// console.log(isIsland);
+// console.log(language);
+
+// // let, const and var
+// language = "English";
+
+// Basic operators
+const currentYear = 2023;
+const agemom = currentYear - 1960;
+const agefidies = currentYear - 1990;
+console.log(agefidies);
+console.log(agemom);
+
+// 2** 3 means 2 to the power of 3 = 2 * 2* 2
+console.log(agefidies * 2, agefidies / 10, 2 ** 3);
+
+// string concatenate
+const firstName = "jane";
+const lastName = "ann";
+console.log(firstName + " " + lastName);
+
+//assignment operators
+let x = 10 + 5; //x = 15
+x += 10; // x = x + 10
+x *= 4; //x = x *4 = 100
+x++; //x = x+1
+x--;
+x--;
+console.log(x);
+
+//comparison operator
+console.log(agefidies > agemom); /// > < >=, <=
+console.log(agefidies >= 18);
